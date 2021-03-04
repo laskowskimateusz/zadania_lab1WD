2e8d52382ef0b2a0caa485fd21cd0347c044d0a4
selfwezmir <self.wezmir@gmail.com>
2021-03-04 18:28:34 +0100

    Add Zad9.py

---
+++ Zad9.py
@@ -0,0 +1,7 @@
+string = "zmienna"
+float = 2.6
+hex = 0xF
+
+print("String - %s, float - %0.1f, hex - %X" % (string, float, hex))
+
+print("String - {}, float - {}, hex - {:X}".format(string, float, hex))
