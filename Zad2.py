0e55275f25706bb8e09659d1c1f77730f8d852f8
selfwezmir <self.wezmir@gmail.com>
2021-03-04 14:12:38 +0100

    Second Commit

---
+++ Zad2.py
@@ -0,0 +1,8 @@
+a, b = 6, 2
+print('Dodawanie - ', a, "+", b, "=", a+b)
+print('Odejmowanie - ', a, "-", b, "=", a-b)
+print('Mnozenie - ', a, "*", b, "=", a*b)
+print('Dzielenie - ', a, "/", b, "=", a/b)
+print('Dzielenie calkowite - ', a, "//", b, "=", a//b)
+print('Potegowanie - ', a, "**", b, "=", a**b)
+print('Reszta z dzielenia - ', a, "%", b, "=", a % b)
