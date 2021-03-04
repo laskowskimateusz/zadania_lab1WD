6b43373884b385fd7d501fdfd54f5a438d225e81
selfwezmir <self.wezmir@gmail.com>
2021-03-04 14:04:14 +0100

    First Commit

---
+++ Zad1.py
@@ -0,0 +1,9 @@
+a, b = 5, 6
+c, d = 6.46, 7.4
+e, f = 'test1', 'test2'
+g, h = 3+6j, 7+5j
+
+print("Integer -", a, ",", b)
+print("Float -", c, ",", d)
+print("String -", e, ",", f)
+print("Complex -", g, ",", h)
