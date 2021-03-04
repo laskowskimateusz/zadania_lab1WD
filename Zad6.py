b52beee3a300fad67bcb88b16a2cbc2bbfcab58f
selfwezmir <self.wezmir@gmail.com>
2021-03-04 14:39:09 +0100

    Add Zad6.py

---
+++ Zad6.py
@@ -0,0 +1,6 @@
+tekst_piosenki = 'la la lo lo li li li li li le le le'
+
+print("Ilosc 'la' -", tekst_piosenki.count("la"))
+print("Ilosc 'lo' -", tekst_piosenki.count("lo"))
+print("Ilosc 'li' -", tekst_piosenki.count("li"))
+print("Ilosc 'le' -", tekst_piosenki.count("le"))
