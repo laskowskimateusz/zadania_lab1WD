1e764a2a4ccb35719898fd74ed3449a253f088b1
selfwezmir <self.wezmir@gmail.com>
2021-03-05 14:37:06 +0100

    Restored Zad8.py

--- Zad9.py
+++ Zad9.py
@@ -5,4 +5,3 @@ hex_int = 0xF
 print("String - %s, float - %0.1f, hex - %X" % (string, float_type, hex_int))
 
 print("String - {}, float - {}, hex - {:X}".format(string, float_type, hex_int))
-print('test2')
