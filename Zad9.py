faf45cea7ca3b9577d9bd75a74f534139cbd0655
selfwezmir <self.wezmir@gmail.com>
2021-03-05 14:39:52 +0100

    Update Zad9.py

--- Zad9.py
+++ Zad9.py
@@ -4,4 +4,4 @@ hex_int = 0xF
 
 print("String - %s, float - %0.1f, hex - %X" % (string, float_type, hex_int))
 
-# ("String - {}, float - {}, hex - {:X}".format(string, float_type, hex_int))
+print("String - {}, float - {}, hex - {:X}".format(string, float_type, hex_int))
